d176712d3f8cd3f20ccfbe710979e3fb2c65d93e
Kuzey Koç <savolla@protonmail.com>
2024-08-28 13:44:53 +0300

    test: testing step by step

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -16,42 +16,42 @@ pipeline{
         git branch: 'devops-pipeline-002-application-source', credentialsId: 'jenkins-credential-github', url: 'https://github.com/savolla/devops-test-repo.git'
       }
     }
-    stage('compile') {
-      steps {
-        sh "mvn compile"
-      }
-    }
-    stage('test') {
-      steps {
-        sh "mvn test"
-      }
-    }
-    stage('trivy fs/dep scan') {
-      steps {
-        sh "trivy fs --format table -o fs.html ."
-      }
-    }
-    stage('sonarqube scan') {
-      steps {
-        withSonarQubeEnv('sonarqube') {
-          sh ''' 
-            $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=Blogging-app -Dsonar.projectKey=Blogging-app \
-            -Dsonar.java.binaries=target
-            '''
-        }
-      }
-    }
-    stage('build') {
-      steps {
-        sh "mvn package"
-      }
-    }
-    stage('publish artifact to nexus') {
-      steps {
-        withMaven(globalMavenSettingsConfig: 'maven-settings', jdk: 'jenkins-tool-jdk17-temurin', maven: 'jenkins-tool-maven', mavenSettingsConfig: '', traceability: true) {
-          sh "mvn deploy"
-        }
-      }
-    }
+    // stage('compile') {
+    //   steps {
+    //     sh "mvn compile"
+    //   }
+    // }
+    // stage('test') {
+    //   steps {
+    //     sh "mvn test"
+    //   }
+    // }
+    // stage('trivy fs/dep scan') {
+    //   steps {
+    //     sh "trivy fs --format table -o fs.html ."
+    //   }
+    // }
+    // stage('sonarqube scan') {
+    //   steps {
+    //     withSonarQubeEnv('sonarqube') {
+    //       sh ''' 
+    //         $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=Blogging-app -Dsonar.projectKey=Blogging-app \
+    //         -Dsonar.java.binaries=target
+    //         '''
+    //     }
+    //   }
+    // }
+    // stage('build') {
+    //   steps {
+    //     sh "mvn package"
+    //   }
+    // }
+    // stage('publish artifact to nexus') {
+    //   steps {
+    //     withMaven(globalMavenSettingsConfig: 'maven-settings', jdk: 'jenkins-tool-jdk17-temurin', maven: 'jenkins-tool-maven', mavenSettingsConfig: '', traceability: true) {
+    //       sh "mvn deploy"
+    //     }
+    //   }
+    // }
   }
 }
