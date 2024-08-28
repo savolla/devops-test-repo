587c4f8f7b990f1e05078e3b85fffe9c7b40acb8
Kuzey Koç <savolla@protonmail.com>
2024-08-28 14:05:13 +0300

    test: tvivy scan

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -26,32 +26,32 @@ pipeline{
         sh "mvn test"
       }
     }
-    // stage('trivy fs/dep scan') {
+    stage('trivy fs/dep scan') {
+      steps {
+        sh "trivy fs --format table -o fs.html ."
+      }
+    }
+    // stage('sonarqube scan') {
     //   steps {
-    //     sh "trivy fs --format table -o fs.html ."
+    //     withSonarQubeEnv('sonarqube') {
+    //       sh ''' 
+    //         $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=Blogging-app -Dsonar.projectKey=Blogging-app \
+    //         -Dsonar.java.binaries=target
+    //         '''
+    //     }
     //   }
     // }
-    stage('sonarqube scan') {
-      steps {
-        withSonarQubeEnv('sonarqube') {
-          sh ''' 
-            $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=Blogging-app -Dsonar.projectKey=Blogging-app \
-            -Dsonar.java.binaries=target
-            '''
-        }
-      }
-    }
-    stage('build') {
-      steps {
-        sh "mvn package"
-      }
-    }
-    stage('publish artifact to nexus') {
-      steps {
-        withMaven(globalMavenSettingsConfig: 'maven-settings', jdk: 'jenkins-tool-jdk17-temurin', maven: 'jenkins-tool-maven', mavenSettingsConfig: '', traceability: true) {
-          sh "mvn deploy"
-        }
-      }
-    }
+    // stage('build') {
+    //   steps {
+    //     sh "mvn package"
+    //   }
+    // }
+    // stage('publish artifact to nexus') {
+    //   steps {
+    //     withMaven(globalMavenSettingsConfig: 'maven-settings', jdk: 'jenkins-tool-jdk17-temurin', maven: 'jenkins-tool-maven', mavenSettingsConfig: '', traceability: true) {
+    //       sh "mvn deploy"
+    //     }
+    //   }
+    // }
   }
 }
